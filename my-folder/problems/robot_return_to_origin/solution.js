e16c954459a2504431adbd77f848879bd147921a
Michael Nguyen <n93michael@gmail.com>
2022-10-17 01:21:26 +0000

    Sync LeetCode submission - Robot Return to Origin (javascript)

---
+++ my-folder/problems/robot_return_to_origin/solution.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function(moves) {
+const setPosition = () => {
+  let x = 0;
+  let y = 0;
+
+  return function (interaction) {
+      interaction.split('').forEach((interaction) => {
+          if (interaction === 'D') {
+        y--
+      } else if (interaction === 'U') {
+        y++
+      } else if (interaction === 'L') {
+        x++
+      } else if (interaction === 'R') {
+        x--
+      } else {
+        return console.log('Please put a valid input');
+      }
+      
+      })
+
+        if (x === 0 && y === 0) {
+            return true
+        } else {
+      
+           return false
+        }
+  }
+}
+
+
+const robot = setPosition()
+return robot(moves)
+};
+
+
+
+
